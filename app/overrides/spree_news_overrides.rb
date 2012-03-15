183703f99355ec87603d1ab54b9b5880bc90b5e3
johndavid400 <johndavid400@yahoo.com>
2012-03-15 09:27:58 -0500

    override

--- app/overrides/spree_news_overrides.rb
+++ app/overrides/spree_news_overrides.rb
@@ -6,4 +6,4 @@ Deface::Override.new(:virtual_path => "spree/layouts/admin",
 Deface::Override.new(:virtual_path => "spree/home/index",
                      :name => "index_news_partial",
                      :insert_after => "[data-hook='homepage_products']",
-                     :partial => "shared/_news")
+                     :partial => "spree/shared/_news")
