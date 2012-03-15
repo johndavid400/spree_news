6ce233eca27f53bc415b8ead0e60903703b1b336
johndavid400 <johndavid400@yahoo.com>
2012-03-15 10:21:21 -0500

    fixing extension

--- app/models/spree/post.rb
+++ app/models/spree/post.rb
@@ -1,4 +1,6 @@
 class Spree::Post < ActiveRecord::Base
   validates_presence_of :description
   validates_length_of :description, :maximum => 150
+
+  scope :published, lambda { where(:published => true) }
 end
--- app/overrides/spree_news_overrides.rb
+++ app/overrides/spree_news_overrides.rb
@@ -1,9 +1,11 @@
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :name => "news_admin_tab",
                      :insert_bottom => "[data-hook='admin_tabs']",
-                     :text => "<%= tab(:posts) %>")
+                     :text => "<%= tab(:posts) %>",
+                     :disabled => false)
 
 Deface::Override.new(:virtual_path => "spree/home/index",
                      :name => "index_news_partial",
                      :insert_after => "[data-hook='homepage_products']",
-                     :partial => "spree/shared/_news")
+                     :partial => "shared/news",
+                     :disabled => false)
