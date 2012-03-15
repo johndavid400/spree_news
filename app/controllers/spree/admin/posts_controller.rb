bdaed8fdb88ab27c581f6b2bb672286811b8d7fe
johndavid400 <johndavid400@yahoo.com>
2012-03-15 13:28:04 -0500

    controller

--- app/controllers/spree/admin/posts_controller.rb
+++ app/controllers/spree/admin/posts_controller.rb
@@ -38,13 +38,15 @@ class Spree::Admin::PostsController < Spree::Admin::ResourceController
     end
   end
 
-#  def destroy
-#    @post = Spree::Post.find(params[:id])
-#    if @post.destroy
-#      redirect_to admin_posts_path
-#    else
-#      redirect_to :back
-#    end
-#  end
+  def destroy
+    @post = Spree::Post.find(params[:id])
+    if @post.destroy
+      flash[:notice] = "Post deleted successfully"
+      redirect_to admin_posts_path
+    else
+      flash[:alert] = "Error deleting post"
+      redirect_to :back
+    end
+  end
 
 end
