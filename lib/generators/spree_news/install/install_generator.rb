7bee12ba3c8f4b7afaa12dc4dafa3810a97beb11
Maxim Rykunov <maxim@rykunov.info>
2013-01-16 14:42:21 +0400

    Update lib/generators/spree_news/install/install_generator.rb
    Fix incorrect renaming in previous commit

--- lib/generators/spree_news/install/install_generator.rb
+++ lib/generators/spree_news/install/install_generator.rb
@@ -3,8 +3,8 @@ module SpreeNews
     class InstallGenerator < Rails::Generators::Base
 
       def add_javascripts
-        append_file "app/assets/javascripts/store/all.js", "//= require store/spree_news\n"
-        append_file "app/assets/javascripts/admin/all.js.coffee", "//= require admin/spree_news\n"
+        append_file "app/assets/javascripts/store/all.js.coffee", "//= require store/spree_news\n"
+        append_file "app/assets/javascripts/admin/all.js", "//= require admin/spree_news\n"
       end
 
       def add_stylesheets
