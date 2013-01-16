4f52541ee4fe40df827354e2695453b0e3b9b740
Maxim Rykunov <maxim@rykunov.info>
2013-01-16 14:36:08 +0400

    Update lib/generators/spree_news/install/install_generator.rb
    Use all.js.coffee instead of all.js

--- lib/generators/spree_news/install/install_generator.rb
+++ lib/generators/spree_news/install/install_generator.rb
@@ -4,7 +4,7 @@ module SpreeNews
 
       def add_javascripts
         append_file "app/assets/javascripts/store/all.js", "//= require store/spree_news\n"
-        append_file "app/assets/javascripts/admin/all.js", "//= require admin/spree_news\n"
+        append_file "app/assets/javascripts/admin/all.js.coffee", "//= require admin/spree_news\n"
       end
 
       def add_stylesheets
