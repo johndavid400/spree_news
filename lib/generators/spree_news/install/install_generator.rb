762d23d7caee0a529887e6905d3ada1e46ec0a6d
Shamir Khodzha <skhodzha@techranch.ru>
2013-07-03 16:53:09 +0400

    fixed coffee tracks

--- lib/generators/spree_news/install/install_generator.rb
+++ lib/generators/spree_news/install/install_generator.rb
@@ -3,7 +3,7 @@ module SpreeNews
     class InstallGenerator < Rails::Generators::Base
 
       def add_javascripts
-        append_file "app/assets/javascripts/store/all.js.coffee", "#= require store/spree_news\n"
+        append_file "app/assets/javascripts/store/all.js", "#= require store/spree_news\n"
         append_file "app/assets/javascripts/admin/all.js", "//= require admin/spree_news\n"
       end
 
